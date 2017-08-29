af2521087348bb6cc6a4f6cc41b17fac1f50cab5
Henley <rah140030@utdallas.edu>
2017-08-29 15:20:16 -0500

    Added first person camera

---
+++ Drop Serene/Assets/EnemyMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    CharacterController controller;
+    public float movementSpeed;
+    [HideInInspector]
+    public float verticalVelocity = 0;
+    public float gravity;
+    public float jumpSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerJumpAndGravity();
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+        Vector3 movementDirection = GameObject.Find("Player").transform.position - transform.position;        
+        transform.rotation = Quaternion.LookRotation(movementDirection);       
+        //keep y rotation to 0?
+
+        controller.Move(transform.forward * Time.deltaTime * movementSpeed);       
+    }
+
+    void playerJumpAndGravity()
+    {
+        verticalVelocity -= gravity * Time.deltaTime;
+        if (verticalVelocity < -gravity * .75F) verticalVelocity = -gravity * .75F;
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            verticalVelocity = jumpSpeed;
+        }
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            if (hit.distance - (GetComponent<CapsuleCollider>().height / 2) < .1F) return true;
+        }
+        return false;
+    }
+}
--- Drop Serene/Assets/PlayerMovement.cs
+++ Drop Serene/Assets/PlayerMovement.cs
@@ -19,16 +19,15 @@ public class PlayerMovement : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update ()
-    { 
-        verticalVelocity -= gravity * Time.deltaTime;
-        if (verticalVelocity < -gravity * .75f) verticalVelocity = -gravity * .75f;
-        if (Input.GetButtonDown("Jump") && grounded(1.1F))
-        {
-            verticalVelocity = jumpSpeed;
-        }
-        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
-        controller.Move(moveVector * Time.deltaTime);
+    {
+        transform.rotation = GameObject.Find("Main Camera").transform.rotation;
+        playerJumpAndGravity();
+        playerMovement();
+    }
 
+
+    void playerMovement()
+    {
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             controller.Move(transform.right * Time.deltaTime * movementSpeed);
@@ -47,14 +46,24 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    bool grounded(float value)
+    void playerJumpAndGravity()
+    {
+        verticalVelocity -= gravity * Time.deltaTime;
+        if (verticalVelocity < -gravity * .75F) verticalVelocity = -gravity * .75F;
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            verticalVelocity = jumpSpeed;
+        }
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+    bool isGrounded()
     {
         RaycastHit hit;
         if (Physics.Raycast(transform.position, -Vector3.up, out hit))
         {
-            Debug.DrawLine(transform.position, hit.point, Color.cyan);
-            Debug.Log(hit.distance);
-            if (hit.distance < value) return true;
+            if (hit.distance - (GetComponent<CapsuleCollider>().height / 2) < .1F) return true;
         }
         return false;
     }
