dbfdee2f325c478d0e05ed1b2038273a12375347
ctknoll <ctk140030@utdallas.edu>
2017-09-05 15:22:42 -0500

    Added a way to making lighting easier for designers

---
+++ Drop Serene/Assets/Scripts/Light/AmbientLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientLight : MonoBehaviour {
+
+	public bool overrideConfigurationWithDefaults;
+
+	// Use this for initialization
+	void Start () 
+	{
+		AmbientLightDefaults defaults = GameObject.Find ("__MASTER__").GetComponent<AmbientLightDefaults>();
+		Light light = gameObject.GetComponent<Light>();
+		if (overrideConfigurationWithDefaults)
+		{
+			light.intensity = defaults.intensity;
+			light.bounceIntensity = defaults.bounce;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+}
---
+++ Drop Serene/Assets/Scripts/Light/AmbientLightDefaults.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientLightDefaults : MonoBehaviour 
+{
+
+	public List<Light> lights;
+	public float intensity;
+	public float bounce;
+	public float range;
+	public bool useDefaultForAll;
+
+	void Start () 
+	{
+		if (useDefaultForAll) 
+		{
+			foreach (Light light in lights) 
+			{
+				light.intensity = intensity;
+				light.bounceIntensity = bounce;
+				light.range = range;
+			}
+		}
+	}
+
+	void Update () {}
+}
