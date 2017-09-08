6baad0e989ecf1b9edcb0a0e52959ac3acdf580c
Krystal <kculp13@gmail.com>
2017-09-07 20:22:03 -0500

    Delete Library folder from repo

---
+++ Drop Serene/Assets/Scripts/Light/Lightswitch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class Lightswitch : MonoBehaviour {  
+    
+    public Light light;    
+    private bool inRange = false;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<BoxCollider>().isTrigger = true;        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire2") && inRange)
+        {
+            light.enabled = !light.enabled;
+        }        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inRange = false;
+    }
+}
