fd22807dba36d5add8ee3b2b80f908a9ce340b43
Katz <sak140130@utdallas.edu>
2017-09-21 20:46:06 -0500

    Added a super rudimentary load door to move the player between areas.

---
+++ Drop Serene/Assets/Scripts/Game Flow/LoadNextLevelArea.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class LoadNextLevelArea : MonoBehaviour {
+    public string levelName;
+	// Use this for initialization
+	void Start () {
+		foreach(Collider col in this.gameObject.GetComponents<Collider>())
+        {
+            col.isTrigger = true;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+}
