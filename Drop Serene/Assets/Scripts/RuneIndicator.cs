084fcd6925677997ae30de4cf3b169a765e59702
Sam Katz <sammykatz@sbcglobal.net>
2017-10-05 09:05:08 -0500

    Added an indicator object to use for multirune doors.

---
+++ Drop Serene/Assets/Scripts/RuneIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneIndicator : MonoBehaviour {
+
+
+    private MeshRenderer indicatorMesh;
+
+    public LightableObject linkedRune;
+
+    public Color deactivatedColor;
+    public Color activatedColor;
+
+	// Use this for initialization
+	void Start () {
+        indicatorMesh = this.gameObject.GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (linkedRune.isActive)
+        {
+            indicatorMesh.material.SetColor("_EmissionColor", activatedColor);
+        }else
+        {
+            indicatorMesh.material.SetColor("_EmissionColor", deactivatedColor);
+        }
+	}
+}
