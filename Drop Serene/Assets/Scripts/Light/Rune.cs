1eb8c38481c59026442d063d1d6c6366e76fb02e
kmjjc <kmc140230@utdallas.edu>
2017-10-16 20:53:03 -0500

    Add alpha build:

--- Drop Serene/Assets/Scripts/Light/Rune.cs
+++ Drop Serene/Assets/Scripts/Light/Rune.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -75,4 +75,4 @@ public class RuneEditor : Editor
 			
 		}
 	}
-}
+}*/
