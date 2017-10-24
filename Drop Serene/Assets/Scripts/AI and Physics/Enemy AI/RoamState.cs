51dec5981cd92733e6d2f16abaecf395043133d7
kmjjc <kmc140230@utdallas.edu>
2017-10-23 19:23:16 -0500

    Fixed monster behavior on levels one and two, actually avoids backtracking now

--- Drop Serene/Assets/Scripts/AI and Physics/Enemy AI/RoamState.cs
+++ Drop Serene/Assets/Scripts/AI and Physics/Enemy AI/RoamState.cs
@@ -29,7 +29,7 @@ public class RoamState : State
 
 	override public void OnStateUpdate()
 	{
-        Debug.DrawLine(goalPos, controller.gameObject.transform.position);
+        Debug.DrawLine(goalPos, controller.gameObject.transform.position, Color.red);
         if (Vector3.Magnitude(controller.agent.transform.position - goalPos) < 2F)
         {
             Vector3 tmpGoal = goalPos;
@@ -65,8 +65,8 @@ public class RoamState : State
             Vector3 randomPosition = (UnityEngine.Random.insideUnitSphere * radius) + pos;
             NavMeshHit hit;
             if (NavMesh.SamplePosition(randomPosition, out hit, 1, NavMesh.AllAreas))
-                if(LightingUtils.inLineOfSight(hit.position, controller.gameObject))
-                    pointsList.Add(new Vector4(hit.position.x, hit.position.y, hit.position.z, localSearchValue(hit.position)));
+                /*if(LightingUtils.inLineOfSight(hit.position, controller.gameObject))*/
+                    pointsList.Add(new Vector4(hit.position.x, hit.position.y, hit.position.z, (float) localSearchValue(hit.position)));
         }
         if (pointsList.Count == 0)
             return localSearch(pos, radius, casts);
@@ -80,28 +80,33 @@ public class RoamState : State
         return hit2.position;
     }
 
-    public float localSearchValue(Vector3 point)
+    public double localSearchValue(Vector3 point)
     {
         //angle between lastGoal and newGoal to player should be greater than 90 (he should avoid backtracking)
         //distance to the new goal should be as far as possible
         //encourage verticality, otherwise he'll never find the strairs (??? unknown if true)
         //distance to goal should be as far from walls as possible
         Vector3 playerPos = controller.gameObject.transform.position;
-        float totalDistance = Vector3.Magnitude(point - playerPos);
-        float yDiff = Mathf.Abs(point.y - controller.gameObject.transform.position.y);
-        float minDistanceToWall = Mathf.Infinity;
-        float angleToLastGoal = Mathf.Abs(Vector3.Angle(playerPos, point) - Vector3.Angle(playerPos, lastGoal));
+        double totalDistance = Vector3.Magnitude(point - playerPos);
+        double yDiff = Mathf.Abs(point.y - controller.gameObject.transform.position.y);
+        double minDistanceToWall = Mathf.Infinity;
+        float angleVal = Vector3.Angle(playerPos - point, playerPos - lastGoal);
+        //angleVal = (float) Sigmoid(Mathf.Sqrt(angleVal) - 9);
         Collider[] checkColliders = Physics.OverlapSphere(point, 10);
         if(checkColliders.Length == 0)
         {
-            return totalDistance + (angleToLastGoal / 10) + 10;
+            return totalDistance * angleVal * 10;
         }
         foreach(Collider c in checkColliders)
         {
             float dist = Vector3.Magnitude(point - c.ClosestPointOnBounds(point));
             if(dist < minDistanceToWall) minDistanceToWall = dist;
         }
-        return totalDistance + (angleToLastGoal / 10) + minDistanceToWall;
+        return totalDistance * angleVal * minDistanceToWall - 2 * yDiff;
     }
 
+    public double Sigmoid(double x)
+    {
+        return 1 / (1 + Math.Exp(-x));
+    }
 }
