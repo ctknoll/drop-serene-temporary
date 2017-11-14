f58a85ef22654981271ed23b6cea9d3b3e66323e
kmjjc <kmc140230@utdallas.edu>
2017-11-14 11:10:13 -0600

    Remove unzipped builds, add pregold_001, fix light settings

--- Drop Serene/Assets/Scripts/Game Flow/SetStartOptions.cs
+++ Drop Serene/Assets/Scripts/Game Flow/SetStartOptions.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class SetStartOptions : MonoBehaviour {
 
     public Slider lightSlider;
+    public Toggle invertMouse;
     AmbientLightDefaults lightScript;
     public float minLightLevel = .02F;
     public float maxLightLevel = .4F;
-    public bool invertMouse = false;
 
 	// Use this for initialization
 	void Start ()
@@ -16,6 +16,7 @@ public class SetStartOptions : MonoBehaviour {
         lightSlider.value = lightScript.intensity;
         lightSlider.minValue = minLightLevel;
         lightSlider.maxValue = maxLightLevel;
+        invertMouse.isOn = PlayerPrefs.GetInt("InvertMouse", 0) == 0 ? false : true;
 	}
 
     public void SetLightLevel()
@@ -25,12 +26,12 @@ public class SetStartOptions : MonoBehaviour {
 
     public void SetMouseInversion()
     {
-        invertMouse = !invertMouse;
+        //invertMouse = !invertMouse;
     }
 
     public void SetPlayerPrefs()
     {
         PlayerPrefs.SetFloat("LightLevel", lightScript.intensity);
-        PlayerPrefs.SetInt("InvertMouse", invertMouse ? 1 : 0);
+        PlayerPrefs.SetInt("InvertMouse", invertMouse.isOn ? 1 : 0);
     }    
 }
